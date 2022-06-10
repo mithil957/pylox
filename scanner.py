98b84a639c82eaca7ab9a3d12a0bd295b4afdf6e
mithil957 <mviradia957@gmail.com>
2022-06-10 13:39:50 -0500

    scanning logic added

---
+++ scanner.py
@@ -0,0 +1,162 @@
+from lox import Lox
+from token import Token, TokenType
+
+
+class Scanner:
+
+    keywords = {
+        'and': TokenType.AND,
+        'class': TokenType.CLASS,
+        'else': TokenType.ELSE,
+        'false': TokenType.FALSE,
+        'for': TokenType.FOR,
+        'fun': TokenType.FUN,
+        'if': TokenType.IF,
+        'nil': TokenType.NIL,
+        'or': TokenType.OR,
+        'print': TokenType.PRINT,
+        'return': TokenType.RETURN,
+        'super': TokenType.SUPER,
+        'this': TokenType.THIS,
+        'true': TokenType.TRUE,
+        'var': TokenType.VAR,
+        'while': TokenType.WHILE
+    }
+
+    def __init__(self, source: str):
+        self.source = source
+        self.tokens: list[Token] = []
+        self.start: int = 0
+        self.current: int = 0
+        self.line: int = 1
+
+    def is_at_end(self) -> bool:
+        return self.current >= len(self.source)
+
+    def scan_tokens(self) -> list[Token]:
+        while not self.is_at_end():
+            self.start = self.current
+            self.scan_token()
+
+        self.tokens.append(Token(TokenType.EOF, "", None, self.line))
+        return self.tokens
+
+    def scan_token(self):
+        c = self.advance()
+        match c:
+            case '(':
+                self.add_token(TokenType.LEFT_PAREN)
+            case ')':
+                self.add_token(TokenType.RIGHT_PAREN)
+            case '}':
+                self.add_token(TokenType.RIGHT_BRACE)
+            case '{':
+                self.add_token(TokenType.LEFT_BRACE)
+            case ',':
+                self.add_token(TokenType.COMMA)
+            case '.':
+                self.add_token(TokenType.DOT)
+            case '+':
+                self.add_token(TokenType.PLUS)
+            case ';':
+                self.add_token(TokenType.SEMICOLON)
+            case '*':
+                self.add_token(TokenType.STAR)
+            case '!':
+                self.add_token(TokenType.BANG_EQUAL if self.match('=') else TokenType.BANG)
+            case '=':
+                self.add_token(TokenType.EQUAL_EQUAL if self.match('=') else TokenType.EQUAL)
+            case '<':
+                self.add_token(TokenType.LESS_EQUAL if self.match('=') else TokenType.LESS)
+            case '>':
+                self.add_token(TokenType.GREATER_EQUAL if self.match('=') else TokenType.GREATER)
+            case '/':
+                if self.match('/'):
+                    while self.peek() != '\n' and not self.is_at_end():
+                        self.advance()
+                else:
+                    self.add_token(TokenType.SLASH)
+            case ' ' | '\r' | '\t':
+                pass
+            case '\n':
+                self.line += 1
+            case '"':
+                self.string()
+            case str(k) if k.isnumeric():
+                self.number()
+            case str(k) if k.isalpha() or k == '_':
+                self.identifier()
+            case _:
+                Lox.error(self.line, "Unexpected character")
+
+    def advance(self) -> str:
+        current_char = self.source[self.current]
+        self.current += 1
+        return current_char
+
+    def add_token(self, token_type: TokenType, literal: object = None):
+        text = self.source[self.start:self.current]
+        self.tokens.append(Token(token_type, text, literal, self.line))
+
+    def match(self, expected: str) -> bool:
+        if self.is_at_end():
+            return False
+        if self.source[self.current] != expected:
+            return False
+
+        self.current += 1
+        return True
+
+    def peek(self) -> str:
+        if self.is_at_end():
+            return '\0'
+        return self.source[self.current]
+
+    def peek_next(self) -> str:
+        if self.current + 1 >= len(self.source):
+            return '\0'
+        return self.source[self.current + 1]
+
+    def string(self):
+        while self.peek() != '"' and not self.is_at_end():
+            if self.peek() == '\n':
+                self.line += 1
+            self.advance()
+
+        if self.is_at_end():
+            Lox.error(self.line, "Unterminated string.")
+
+        # The closing "
+        self.advance()
+
+        value = self.source[self.start + 1: self.current - 1]
+        self.add_token(TokenType.STRING, value)
+
+    def number(self):
+        while self.peek().isnumeric():
+            self.advance()
+
+        if self.peek() == '.' and self.peek_next().isnumeric():
+            self.advance()
+
+            while self.peek().isnumeric():
+                self.advance()
+
+        # TODO float conversion could throw error
+        self.add_token(TokenType.NUMBER, float(self.source[self.start: self.current]))
+
+    @staticmethod
+    def is_acceptable_char(c: str) -> bool:
+        return c.isalnum() or '_'
+
+    def identifier(self):
+        while Scanner.is_acceptable_char(self.peek()):
+            self.advance()
+
+        text = self.source[self.start: self.current]
+
+        match Scanner.keywords.get(text, None):
+            case None:
+                self.add_token(TokenType.IDENTIFIER)
+            case _ as token_type:
+                self.add_token(token_type)
